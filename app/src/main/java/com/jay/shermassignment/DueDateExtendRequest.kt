4316c6fd3a7fa734b27a1c7c2c55f684734e89e1
Jaykumar Vaghasiya <139869772+JaykumarVaghasiya@users.noreply.github.com>
2023-10-10 17:31:29 +0530

    Inspection Details UI and List

---
+++ app/src/main/java/com/jay/shermassignment/DueDateExtendRequest.kt
@@ -0,0 +1,11 @@
+package com.jay.shermassignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DueDateExtendRequest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_due_date_extend_request)
+    }
+}
