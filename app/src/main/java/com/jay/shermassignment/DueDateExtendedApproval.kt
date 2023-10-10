060a87c18ba7a081849a110f6b118fadba605a7e
Jaykumar Vaghasiya <139869772+JaykumarVaghasiya@users.noreply.github.com>
2023-10-10 19:14:17 +0530

    Inspection Module UI

---
+++ app/src/main/java/com/jay/shermassignment/DueDateExtendedApproval.kt
@@ -0,0 +1,11 @@
+package com.jay.shermassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DueDateExtendedApproval : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_due_date_extended_approval)
+    }
+}
